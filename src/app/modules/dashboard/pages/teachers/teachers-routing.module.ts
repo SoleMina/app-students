73ce08abbf1df98553c9d84658201c9b0c7d0fa6
Karina Prado <karina.pradogutierrez@gmail.com>
2025-02-20 12:29:34 -0500

    feat: update components

--- src/app/modules/dashboard/pages/teachers/teachers-routing.module.ts
+++ src/app/modules/dashboard/pages/teachers/teachers-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TeachersComponent } from './teachers.component';
 import { TeacherFormComponent } from './components/teacher-form/teacher-form.component';
-import { TeacherDetailsComponent } from './teacher-details/teacher-details.component';
+import { TeacherDetailsComponent } from './components/teacher-details/teacher-details.component';
 
 const routes: Routes = [
   {
